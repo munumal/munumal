6445161895704643478bfe91edc92ed753cd914b
munumal <101518116+munumal@users.noreply.github.com>
2024-09-25 21:09:56 +0530

    usecase.py

--- usecase.py
+++ usecase.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 25 15:02:04 2024
-
-@author: Musaddique
-"""
-
-# example_usage.py
 
 from basic_operations import add, subtract, multiply, divide
 from advanced_operations import power, square_root
 from trigonometric_operations import sine, cosine, tangent
+from geometry import circle, rectangle
 
+
+print(circle(5))
+print(rectangle(10,5))
 print(add(5, 3))  
 print(square_root(16))  
 print(sine(30))  
